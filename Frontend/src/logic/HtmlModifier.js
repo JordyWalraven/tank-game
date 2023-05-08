12cd756fd5214e035afbee294e69d3d03327b0cb
Jordy Walraven <jordywalraven1@gmail.com>
2023-05-08 16:12:22 +0200

    Updating logic

--- Frontend/src/logic/HtmlModifier.js
+++ Frontend/src/logic/HtmlModifier.js
@@ -4,15 +4,23 @@ class HtmlModifier {
 
   updateBeforeGameMenu(menuInfo) {
     const playerCount = menuInfo.names.length;
+    let playerAmount = 0;
+    for (let index = 0; index < playerCount; index++) {
+      if (menuInfo.names[index] !== '') {
+        playerAmount++;
+      }
+    }
     const names = menuInfo.names;
     const playerList = document.getElementById('playerNames');
     const playerCountElement = document.getElementById('playerCount');
-    playerCountElement.innerHTML = `Amount of players : ${playerCount}`;
+    playerCountElement.innerHTML = `Amount of players : ${playerAmount}`;
     playerList.innerHTML = '';
     for (let i = 0; i < playerCount; i++) {
-      const player = document.createElement('li');
-      player.innerHTML = names[i];
-      playerList.appendChild(player);
+      if (names[i] !== '') {
+        const player = document.createElement('li');
+        player.innerHTML = names[i];
+        playerList.appendChild(player);
+      }
     }
   }
 
