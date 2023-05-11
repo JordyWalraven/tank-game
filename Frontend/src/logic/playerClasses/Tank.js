a84813545e1e1bc7bf6d255ce81d186fa652df4c
Jordy Walraven <jordywalraven1@gmail.com>
2023-05-11 12:04:30 +0200

    Adding UI v1

--- Frontend/src/logic/playerClasses/Tank.js
+++ Frontend/src/logic/playerClasses/Tank.js
@@ -8,7 +8,7 @@ class Tank {
     this.yPos = 0;
     this.groundRef = groundRef;
     this.canvasRef = canvasRef;
-    this.ctx = canvasRef.getContext('2d');
+    this.ctx = canvasRef.getContext("2d");
     this.color = color;
     this.angle = angle;
     this.power = power;
@@ -19,6 +19,8 @@ class Tank {
     this.xPos = x;
     if (this.xPos > 2558 - 25) {
       this.xPos = 2558 - 25;
+    } else if (this.xPos < 25) {
+      this.xPos = 25;
     }
 
     this.yPos = this.groundRef.points[this.xPos].y - 50;
@@ -43,16 +45,11 @@ class Tank {
     this.ctx.translate(this.xPos, this.yPos);
     this.ctx.rotate(this.angle * (Math.PI / 180));
     this.ctx.rect(0, -7.5, 60, 15);
-    this.ctx.fillStyle = 'red';
+    this.ctx.fillStyle = "red";
     this.ctx.fill();
     this.ctx.restore();
     this.ctx.closePath();
-    this.ctx.font = '20px Arial';
-    this.ctx.fillStyle = 'white';
-    this.ctx.fillText(`Current Angle: ${Math.abs(this.angle % 360)}, Current Power: ${this.power}`, this.xPos - 80, this.yPos - 100);
   }
-
 }
 
 module.exports = Tank;
-
